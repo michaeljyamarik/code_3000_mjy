fd60a1f5c29f1406ff837beebe3a461d06191a4c
ID5 DERT <michael.yamarik@gmail.com>
2026-03-11 10:19:12 -0400

    yeah

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -18,7 +18,23 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    keys = ["age", "zip3", "gender"]
+
+    anon_counts = anon_df.groupby(keys).size().reset_index(name = "anon_count")
+
+    aux_counts = aux_df.groupby(keys).size().reset_index(name="aux_count")
+
+    unique_keys = pd.merge(anon_counts, aux_counts, on=keys)
+
+    unique_keys = unique_keys[(unique_keys["anon_count"] == 1) * (unique_keys["aux_count"] == 1)][keys]
+
+    anon_unique = pd.merge(anon_df, unique_keys, on=keys)
+
+    aux_unique = pd.merge(aux_df, unique_keys, on=keys)
+
+    matches = pd.merge(anon_unique, aux_unique, on=keys)
+
+    return matches[["anon_id", "name"]].rename(columns={"name": "matched_name"})
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +42,4 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    return len(matches_df) / len(anon_df)
